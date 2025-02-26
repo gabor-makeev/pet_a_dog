123c27d928821023f625a7ec39d608258cc2cfd4
Gabor Makeev <gabor.makejev@gmail.com>
2025-02-26 20:46:21 +0100

    implement light and dark themes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import {ModeToggle} from "@/components/ModeToggle";
 
 export default function Home() {
   return (
@@ -95,6 +96,7 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
+        <ModeToggle />
       </footer>
     </div>
   );
